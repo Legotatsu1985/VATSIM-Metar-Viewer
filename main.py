083f1a3587d1bc2e4a61c79e40240e1009e30af5
MENON1939 <ryotaro1985@outlook.com>
2023-12-07 20:36:17 +0900

    finding temp convert

--- main.py
+++ main.py
@@ -86,9 +86,8 @@ def metar_find_visibility(list_metar):
         metar_visibility.config(text="地上視程(m): " + metar_visibility_raw)
 
 def metar_find_temp_dewpoint(metar_string_text):
-    if re.search(r'(^[0-9]{2}/[0-9]{,3})', metar_string_text):
-        res = re.search(r'(^[0-9]{2}/[0-9]{,3})', metar_string_text)
-        print(res.group())
+    if re.search(r'([0-9]{2}/[0-9]{,3})', metar_string_text):
+        res = re.search(r'([0-9]{2}/[0-9]{,3})', metar_string_text)
         temp = res.group()[:2]
         dewpoint = res.group()[3:]
         metar_temp.config(text="気温: " + temp + "C")
